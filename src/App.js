73bd7393008411544f5c1bcb5c434956f1b55fde
Zhen <william.zhen.yan@gmail.com>
2020-06-23 21:23:17 +0900

    sample input form

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import InputForm from './InputForm'
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <InputForm />
+  </div>
+);
+
+export default App;
